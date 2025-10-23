6761228e924e7061b9ac6cbab338e443bc6213d5
n3n3 <nene.david.019@gmail.com>
2025-10-23 13:47:38 +0300

    Testsx, scripts

---
+++ test/BasicNft.t.sol
@@ -0,0 +1,19 @@
+// SPDX- License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft
+
+    function setUp() public {
+        deployer= new DeployBasicNft();
+        basicNft = deployer.run();
+
+    }
+}
